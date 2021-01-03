65fbefecf638a02ceeaa555bfa14e0e55de5bb7c
Dwap <mshan2923@gmail.com>
2021-01-03 19:33:47 +0900

    Add CollectionList  - ProperyDrawer - Base

--- Assets/Script/VariableCollection/TestCollection.cs
+++ Assets/Script/VariableCollection/TestCollection.cs
@@ -1,21 +1,96 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class TestCollection : MonoBehaviour
 {
-    VariableCollection.CollectionList collection = new VariableCollection.CollectionList();
+    public VariableCollection.CollectionList collection = new VariableCollection.CollectionList();
     // Start is called before the first frame update
     void Start()
     {
         collection.Add<int>(10);
+        collection.Add<string>("Testing");
 
         print(collection.Get<int>(0));
+        print(collection.Get<string>(1));
+
+        print(GetTypeFromAssemblies(typeof(int).FullName));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(collection.Get<int>(0));
     }
+
+    public static Type GetTypeFromAssemblies(string TypeName)
+    {
+        // null 반환 없이 Type이 얻어진다면 얻어진 그대로 반환.
+        var type = Type.GetType(TypeName);
+        if (type != null)
+            return type;
+
+        // 프로젝트에 분명히 포함된 클래스임에도 불구하고 Type이 찾아지지 않는다면,
+        // 실행중인 어셈블리를 모두 탐색 하면서 그 안에 찾고자 하는 Type이 있는지 검사.
+        var currentAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+        var referencedAssemblies = currentAssembly.GetReferencedAssemblies();
+        foreach (var assemblyName in referencedAssemblies)
+        {
+            var assembly = System.Reflection.Assembly.Load(assemblyName);
+            if (assembly != null)
+            {
+                // 찾았다 요놈!!!
+                type = assembly.GetType(TypeName);
+                if (type != null)
+                    return type;
+            }
+        }
+
+        // 못 찾았음;;; 클래스 이름이 틀렸던가, 아니면 알 수 없는 문제 때문이겠지...
+        return null;
+    }
+}
+
+
+[CustomEditor (typeof(TestCollection))]
+class TestCollectionInspector : Editor
+{
+    SerializedProperty property;
+    TestCollection collection;
+
+    private void OnEnable()
+    {
+        property = serializedObject.FindProperty("collection");
+        collection = target as TestCollection;
+    }
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        /*
+        serializedObject.Update();
+
+        serializedObject.ApplyModifiedProperties();
+
+        if (GUI.changed)
+            EditorUtility.SetDirty(target);
+        */
+        EditorGUILayout.HelpBox("Test", MessageType.None);
+    }
+    
+
+    void DrawList(SerializedProperty _list, string _labalName)
+    {
+        //리스트 갯수 표시
+        EditorGUILayout.PropertyField(_list.FindPropertyRelative("Array.size"), new GUIContent("리스트 갯수 표시"));
+        int Count = _list.arraySize;
+
+        if(_list.arraySize > 0)
+        for (int i = 0; i < Count; ++i)
+        {
+            EditorGUILayout.PropertyField(_list.GetArrayElementAtIndex(i), new GUIContent(_labalName + i));
+        }
+    }//출처: https://rbals1101.tistory.com/75 [프로그래밍 공부!!]
 }
--- Assets/Script/VariableCollection/VariableCollection.cs
+++ Assets/Script/VariableCollection/VariableCollection.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using System;
+using UnityEngine.UIElements;
 
+[System.Serializable]
 public class VariableCollection
 {
+    [System.Serializable]
     class Wrap<T>
     {
         [SerializeField]
@@ -16,15 +22,19 @@ public class VariableCollection
 
         public T Get { get => data; }
     }//이렇게 감싸주면 배열,리스트 등을 직렬화가능 
+
+    [System.Serializable]
     public class CollectionList
     {
         [SerializeField]
-        List<string> Data;
-        //타입을 저장해서 쓸수 있지 않을까?
+        public List<string> Data;
+        [SerializeField]
+        public List<System.Type> DataType;//String 으로 바꿔야 ProperyDrawer 에서 사용 가능
 
         public CollectionList()
         {
             Data = new List<string>();
+            DataType = new List<Type>();
         }
         public bool Set<T>(int index, T vaule)
         {
@@ -36,6 +46,7 @@ public class VariableCollection
                 if (index < Data.Count && index >= 0 && Data.Count != 0)
                 {
                     Data[index] = data;
+                    DataType[index] = typeof(T);
                     return true;
                 }
                 else
@@ -53,6 +64,7 @@ public class VariableCollection
             Wrap<T> wrap = new Wrap<T>(vaule);
             string data = JsonUtility.ToJson(wrap);
             Data.Add(data);
+            DataType.Add(typeof(T));
         }
         public T Get<T>(int index)
         {
@@ -65,5 +77,107 @@ public class VariableCollection
                 return default;
             }
         }
+        public List<T> Gets<T>()
+        {
+            var indexs = GetEqualTypes<T>();
+            List<T> Vaules = new List<T>();
+            for(int i = 0; i < indexs.Count; i++)
+            {
+                Vaules.Add(Get<T>(i));
+            }
+
+            return Vaules;
+        }
+        public Type GetType(int index)
+        {
+            return DataType[index];
+        }
+        public int Find<T>(T vaule)
+        {
+            List<int> indexs = new List<int>();
+            for(int i = 0; i < DataType.Count; i++)
+            {
+                if(DataType[i] == typeof(T))
+                {
+                    //indexs.Add(i);
+                    T temp = Get<T>(i); 
+                    if(vaule.Equals(temp))
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+        public List<int> GetEqualTypes<T>()
+        {
+            List<int> indexs = new List<int>();
+            for (int i = 0; i < DataType.Count; i++)
+            {
+                if (DataType[i] == typeof(T))
+                {
+                    indexs.Add(i);
+                }
+            }
+            return indexs;
+        }
+    }
+}
+
+[CustomPropertyDrawer(typeof(VariableCollection.CollectionList))]
+public class CollectionListProperty : PropertyDrawer
+{
+    public override bool CanCacheInspectorGUI(SerializedProperty property)
+    {
+        return base.CanCacheInspectorGUI(property);
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        float temp = base.GetPropertyHeight(property, label);
+        temp += GetArrayHeight(property, "Data", 25, 0);
+
+        return temp;
+    }
+    public float GetArrayHeight(SerializedProperty property , string name = "Data", float indexSize = 25 , float Default = 50)
+    {
+        var Local = property.FindPropertyRelative(name);
+        int size = Mathf.Clamp(Local.arraySize, 1, Local.arraySize) + 1;//최소 2칸 , 갯수가 1개이상 - 갯수 + 1
+        return Local.isExpanded ? size * indexSize + Default : Default;
+    }
+    
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //base.OnGUI(position, property, label);
+        EditorGUI.BeginProperty(position, label, property);
+
+        EditorGUI.PropertyField(position, property.FindPropertyRelative("Data"), new GUIContent());
+        if(property.FindPropertyRelative("DataType") != null)
+        {
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("DataType"), new GUIContent());
+        }
+        else
+        {
+            Debug.LogWarning("DataType is null??");
+        }
+        //해당 타입으로 변환해서 보여주는거....
+
+        EditorGUI.EndProperty();
+
+        GetData(position, property);
+    }
+
+    public void GetData(Rect position, SerializedProperty property)
+    {
+        int Count = property.FindPropertyRelative("Data").arraySize;
+        for(int i = 0; i < Count; i++)
+        {
+            string text = property.FindPropertyRelative("Data").GetArrayElementAtIndex(i).stringValue;
+            //Type type = property.FindPropertyRelative("DataType").GetArrayElementAtIndex(i).name.GetType();
+            Debug.Log(property.FindPropertyRelative("DataType") + " // " + text);
+            
+        }
+        //property.FindPropertyRelative("Data").GetArrayElementAtIndex(0);
     }
 }
