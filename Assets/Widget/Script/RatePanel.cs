31b46fa5625b514115fd79c55c9494fe03ad8407
mshan2923 <mshan2923@naver.com>
2021-09-05 20:14:25 +0900

    Update 21.1.19 / RatePanel

--- Assets/Widget/Script/RatePanel.cs
+++ Assets/Widget/Script/RatePanel.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class RatePanel : MonoBehaviour
 {
+    [Header("Rect Vaule is Clamp 0 ~ 1 / Origin : LeftButtom")]
     public Map<GameObject, Rect> ObjRect = new Map<GameObject, Rect>();
-    //헤상도 , 해상도 변경알림
+    //해상도 변경알림 -> 대신 다시 활성화 될때 변경확인
 
+    public bool ForcePivot_LeftTop = false;//자식위젯 피벗을 모두 좌상단으로 변환
+    public bool SetParentCanvas = false;
+
+    Vector2 LastResolution = new Vector2();
+    private void OnEnable()
+    {
+        Vector2 Temp = new Vector2(Screen.width, Screen.height);
+
+        if (LastResolution != Temp)
+        {
+            LastResolution = Temp;
+
+            UIUpdate();
+        }
+    }
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UIUpdate(bool UseCanvasSize = false)//UseCanvasSize -> Before Play
     {
-        
+        for (int i = 0; i < ObjRect.Count; i++)
+        {
+            var RectT = ObjRect.GetKey(i).GetComponent<RectTransform>();
+
+            if (ForcePivot_LeftTop)
+            {
+                RectT.pivot = new Vector2(0, 1);
+            }
+
+            if (UseCanvasSize || SetParentCanvas)
+            {
+                //LastResolution = new Vector2(Screen.width, Screen.height);
+                LastResolution = gameObject.GetComponentInParent<Canvas>().pixelRect.size;                
+            }
+
+            ObjRect.GetKey(i).transform.position = ObjRect.GetVaule(i).position * LastResolution;
+            RectT.sizeDelta = ObjRect.GetVaule(i).size * LastResolution;
+
+        }
+    }
+}
+
+[UnityEditor.CustomEditor(typeof(RatePanel))]
+public class RatePanelEditor : UnityEditor.Editor
+{
+    RatePanel owner;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        owner = target as RatePanel;
+
+        if (GUILayout.Button("Update"))
+        {
+            owner.UIUpdate(true);
+        }
     }
 }
